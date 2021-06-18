1ab11362f1e63676cef83938771a709c97b91015
Edson Angoti <angoti@gmail.com>
2021-06-18 19:10:16 -0300

    @CrossOrigin at AuthResource

--- src/main/java/com/educandoweb/course/resources/AuthResource.java
+++ src/main/java/com/educandoweb/course/resources/AuthResource.java
@@ -4,6 +4,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +15,7 @@ import com.educandoweb.course.dto.EmailDTO;
 import com.educandoweb.course.dto.TokenDTO;
 import com.educandoweb.course.services.AuthService;
 
+@CrossOrigin
 @RestController
 @RequestMapping(value = "/auth")
 public class AuthResource {
