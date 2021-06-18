e447ce4bab2156a6bf0bd243eb3f151b3534a818
Edson Angoti <angoti@gmail.com>
2021-06-18 20:28:03 -0300

    http.cors();

--- src/main/java/com/educandoweb/course/security/WebSecurityConfig.java
+++ src/main/java/com/educandoweb/course/security/WebSecurityConfig.java
@@ -9,7 +9,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().anyRequest().authenticated().and().httpBasic();
+		//http.authorizeRequests().anyRequest().authenticated().and().httpBasic();
 		http.cors();
 	}
 
